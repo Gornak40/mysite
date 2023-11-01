b227b66124c66f8f0648f030753a511a0f2fa468
Alexander Gorbunov <s-kozelsk@yandex.ru>
2023-11-02 01:13:51 +0300

    feat: empty landing

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-import os
-
-from flask import Flask, url_for, render_template
+from config import config
+from flask import Flask, render_template, redirect
 
 
 app = Flask(__name__)
@@ -8,7 +7,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html', title='Home')
+	return render_template('index.html', title='Home', config=config)
+
+
+@app.route('/cli-tools')
+def cli_tool():
+	return render_template('cli_tools.html', title='CLI Tools', config=config)
+
+
+@app.route('/tinkoff')
+def tinkoff():
+	return redirect('https://algocourses.ru')
+
+
+@app.route('/contact')
+def contact():
+	return redirect('mailto:s-kozelsk@yandex.ru')
 
 
 if __name__ == '__main__':
---
+++ config.py
@@ -0,0 +1,6 @@
+config = [
+	('Home', '/'),
+	('CLI Tools', '/cli-tools'),
+	('Tinkoff', 'https://algocourses.ru'),
+	('Contact', '/contact'),
+]
