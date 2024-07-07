321c94490314f13667714c7c38db6b5f48aeec3b
Gornak40 <s-kozelsk@yandex.ru>
2024-07-07 23:03:55 +0300

    feat: riscv ejudge

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 config = [
 	('CLI Tools', '/cli-tools'),
 	('CrossPawn', '/crosspawn'),
+	('RISC-V Ejudge', 'https://ejudge.algolymp.ru/serve-control'),
 	('AVX Contest', 'https://algocourses.ru/avx2024'),
 	('T-Gen', 'https://algocourses.ru'),
 ]
