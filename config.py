9c428a2a804030f1a6794e2a5347e57317e9c12c
Gornak40 <s-kozelsk@yandex.ru>
2024-09-18 13:19:33 +0300

    chore: smaller names

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 config = [
 	('CLI Tools', '/cli-tools'),
 	('CrossPawn', '/crosspawn'),
-	('RISC-V Contest', 'https://ejudge.algolymp.ru/cgi-bin/new-register?contest_id=1010'),
-	('AVX Contest', 'https://algocourses.ru/avx2024'),
+	('RISC-V', 'https://ejudge.algolymp.ru/cgi-bin/new-register?contest_id=1010'),
+	('AVX', 'https://algocourses.ru/avx2024'),
 	('T-Gen', 'https://algocourses.ru'),
-	('Flashka Archive', 'ftp://algolymp.ru'),
+	('Flashka', 'ftp://algolymp.ru'),
 ]
