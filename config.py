44a3acee8e4093d6063eb81a9df26b69d92b1c2b
Gornak40 <s-kozelsk@yandex.ru>
2024-09-18 13:17:25 +0300

    chore: add ftp link; fix riscv link

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 config = [
 	('CLI Tools', '/cli-tools'),
 	('CrossPawn', '/crosspawn'),
-	('RISC-V Ejudge', 'https://ejudge.algolymp.ru/serve-control'),
+	('RISC-V Contest', 'https://ejudge.algolymp.ru/cgi-bin/new-register?contest_id=1010'),
 	('AVX Contest', 'https://algocourses.ru/avx2024'),
 	('T-Gen', 'https://algocourses.ru'),
+	('Flashka Archive', 'ftp://algolymp.ru'),
 ]
